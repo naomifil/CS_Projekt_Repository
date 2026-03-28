eadaddfa7cd5e7a7c2bc793a71367b5674d575ca
naomifil <271601648+naomifil@users.noreply.github.com>
2026-03-28 16:07:02 +0100

    Set up of the project scaffold

--- app_main_file.py
+++ app_main_file.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.write ("Welcome to our App")
-st.write ("This is a tool to help yyou book your business trip")
+st.write ("This is a tool to help you book your business trip")
 
 st.write ("Please enter your destination to get started")

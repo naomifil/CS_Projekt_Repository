e4351bba113b9d33ee5420aceb909a63a2036365
naomifil <271601648+naomifil@users.noreply.github.com>
2026-03-29 11:15:32 +0200

    minor changes in text

--- app_main_file.py
+++ app_main_file.py
@@ -5,7 +5,8 @@ import os
 st.write ("Welcome to our App")
 st.write ("This is a tool to help you book your business trip")
 
-st.write ("Please enter your destination to get started")
+st.write (""" Please enter your departure place, destination office and start date to get started. 
+          We'll show you your flight options. """)
 
 # How to load data from .env file
 load_dotenv()

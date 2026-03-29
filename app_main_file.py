8974cf666bcc08cc2fd754536b8f414ee0039563
naomifil <271601648+naomifil@users.noreply.github.com>
2026-03-29 11:09:54 +0200

    Integrate dotenv for API key management
    Added dotenv support to load API key from .env file.

--- app_main_file.py
+++ app_main_file.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
 
 st.write ("Welcome to our App")
 st.write ("This is a tool to help you book your business trip")
 
 st.write ("Please enter your destination to get started")
+
+# How to load data from .env file
+load_dotenv()
+ api_key = os.getenv("API_KEY")
